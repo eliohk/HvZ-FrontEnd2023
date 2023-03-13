ebf5d2f8a84d510d3d0870b0e3e90034ef756669
Mostafa-Mohammedi <121897256+Mostafa-Mohammedi@users.noreply.github.com>
2023-03-13 11:26:02 +0100

    moved the chat and player list to different component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import GameDetails from './components/gameDetailsComponent/GameDetails';
 import TopNavbar from "./components/navigationComponent/TopNavbar";
 import Register from './components/registerComponent/Register';
+import GameDetailsPage from './pages/gameDetailsPage/GameDetailsPage';
 
 function App() {
   return (
     <div>
-      <Register/>
+      <GameDetailsPage/>
     </div>
   );
 }

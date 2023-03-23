c1aea6cbe01ade07e800ef35b11bb09628f3fab2
AdamKrason <57142578+AdamKrason@users.noreply.github.com>
2023-03-23 13:45:54 +0100

    Revert "fixed user stuff"

--- src/App.js
+++ src/App.js
@@ -3,19 +3,11 @@ import TopNavbar from "./components/navigationComponent/TopNavbar";
 
 import { useDispatch } from 'react-redux';
 import { fetchGames } from './states/dataSlice';
-import keycloak from './keycloak';
-import { fetchUserByToken } from './states/dataSlice';
 
 function App() {
   
   const dispatch = useDispatch();
   dispatch(fetchGames());
-
-  const userObj = {
-    userToken: keycloak.idTokenParsed.sub,
-    userName: keycloak.tokenParsed.preferred_username
-  }
-  dispatch(fetchUserByToken(userObj))
 
   return (
     <div className="App">
